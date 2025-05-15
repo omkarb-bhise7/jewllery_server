a5aa89e3a7fd0e97a049fd485ccfbef45e2a444b
omkarb-bhise7 <1999omkarbhise@gmail.com>
2025-05-15 15:29:11 +0530

    .js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // ✅ Enable CORS
 app.use(cors({
-  origin: 'https://bakers-client.onrender.com', // 🔁 You can change this to '*' for testing
+  origin: 'https://jewllery-server.onrender.com', // 🔁 You can change this to '*' for testing
   credentials: true
 }));
 
